936ce6070fa86c122e43ae749b5b2dbba3b26dc9
Tedros Alem <tedsaasfaha@gmail.com>
2024-09-27 20:40:41 +0300

    front demo - CarRental demo implemented

---
+++ src/front_demo/CarRental.java
@@ -0,0 +1,41 @@
+package front_demo;
+
+import java.util.Scanner;
+
+public class CarRental {
+    private final Scanner scn;
+
+    public CarRental() {
+        scn = new Scanner(System.in);
+
+        homePage();
+    }
+
+    private void homePage() {
+        int choice;
+
+        do {
+            System.out.println("*** Car Rental Management System ***");
+            System.out.println("1. Car Management");
+            System.out.println("2. Customer Management");
+            System.out.println("0. Exit");
+            System.out.println("Enter your Choice: ");
+            choice = scn.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Car Management");
+                    break;
+                case 2:
+                    System.out.println("Customer Management");
+                    break;
+                case 0:
+                    System.out.println("Exiting from system...");
+                    break;
+                default:
+                    System.out.println("Please enter a valid input!");
+            }
+        } while(choice != 0);
+    }
+
+}
